f420b903cb47246e0ca91516cb05db17f479c02b
LibiaMLany <82403984+libialany@users.noreply.github.com>
2023-02-13 17:35:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,25 +8,42 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
+function converttounix(date) {
+  return Math.floor(new Date(date).getTime())// / 1000)
+}
+function converttodate(unixdate) {
+  let aux=unixdate/1000
+  return new Date(aux *1000);
+}
+app.get("/api/:date?", function(req, res) {
+  let { date } = req.params;
+  let timeInMillisecond;
+  let timeInDate;
+  if (date.includes('-')) {
+    timeInDate = new Date(date).toUTCString()
+    let curdate = new Date(date).getTime();
+    if (!curdate) {
+      return res.json({ error: "Invalid Date" });
+    }
+    timeInMillisecond = converttounix(date);
+  } else {
+    timeInMillisecond = parseInt(date)
+    timeInDate = converttodate(date).toUTCString()
+  }
+  res.json({ unix: timeInMillisecond, utc: timeInDate })
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
